e777d02a7437ea4b78431d824ea70c1cc4253ef9
Bobby Impollonia <bobbyi@gmail.com>
2013-03-30 20:08:10 -0700

    Add support for enter key. Clear text on submit.

--- src/com/example/todoapp/MainActivity.java
+++ src/com/example/todoapp/MainActivity.java
@@ -2,17 +2,32 @@ package com.example.todoapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnKeyListener;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    	editText = (EditText)findViewById(R.id.edit_message);
+
+    	editText.setOnKeyListener(new OnKeyListener() {
+    		public boolean onKey(View v, int keyCode, KeyEvent event) {
+    			// If the event is a key-down event on the "enter" button
+    			if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+    				addItem();
+    				return true;
+    			}
+    			return false;
+    		}
+    	});
     }
 
     @Override
@@ -26,9 +41,13 @@ public class MainActivity extends Activity {
     	Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
-    public void addItem(View view) {
-    	EditText editText = (EditText)findViewById(R.id.edit_message);
+    public void addItem() {
     	String text = editText.getText().toString();
     	toast(text);
+    	editText.setText("");
+    }
+ 
+    public void onButtonClick(View view) {
+    	addItem();
     }
 }
