d619f6f97c3d5815fa48ca21639a30552aa0b1ce
Bobby Impollonia <bobbyi@gmail.com>
2013-03-31 22:32:14 -0700

    Add button to copy text with newline to clipboard
    For debugging purposes
    Use the deprecated android.text.ClipboardManager class to work with the
    clipboard since that works back to Android 2.2

--- src/com/example/todoapp/MainActivity.java
+++ src/com/example/todoapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.todoapp;
 
 import android.app.Activity;
+import android.text.ClipboardManager;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -9,14 +10,17 @@ import android.view.View.OnKeyListener;
 import android.widget.EditText;
 import android.widget.Toast;
 
+@SuppressWarnings("deprecation")
 public class MainActivity extends Activity {
-    EditText editText;
+    private EditText editText;
+    private ClipboardManager clipboardManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editText = (EditText)findViewById(R.id.edit_message);
+        clipboardManager = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE); 
 
         createEnterKeyListener();
     }
@@ -59,5 +63,9 @@ public class MainActivity extends Activity {
  
     public void onButtonClick(View view) {
         addItem();
+    }
+
+    public void copyText(View view) {
+        clipboardManager.setText("Text\nwith\nnewlines");
     }
 }
